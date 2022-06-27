da5f78c8f19269df6e4b07eaf13495a1b5619075
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-06-27 21:41:25 +0100

    Updated Blockly toolbox to include numbers, math operations and loops

--- blockly-react/src/components/blockly/toolbox.ts
+++ blockly-react/src/components/blockly/toolbox.ts
@@ -26,6 +26,23 @@ let toolbox = {
       },
       {
         "kind": "category",
+        "name": "Loops",
+        "colour": "300",
+        "contents": [
+          {
+            "kind": "block",
+            "type": "controls_for",
+            "fields": {
+              "VAR": {
+                "name": "index",
+                "type": "Number"
+              }
+            } 
+          }
+        ]
+      },
+      {
+        "kind": "category",
         "name": "Control",
         "colour": "210",
         "contents": [
@@ -33,6 +50,25 @@ let toolbox = {
             "kind": "block",
             "type": "controls_if"
           },
+        ]
+      },
+      {
+        "kind": "category",
+        "name": "Math",
+        "colour": "0",
+        "contents": [
+          {
+            "kind": "block",
+            "type":"math_number"
+          },
+          {
+            "kind": "block",
+            "blockxml": "<block type='math_arithmetic'><field name='OP'>ADD</field></block>"
+          },
+          {
+            "kind": "block",
+            "blockxml": "<block type='math_arithmetic'><field name='OP'>MINUS</field></block>"
+          }
         ]
       },
       {
