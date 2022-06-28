3c76fefba0ad07d44d2614a534be73e8ad8cabe9
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-06-28 09:39:46 +0100

    Update Navbar: Code quality improvements

--- blockly-react/src/components/navbar/Navigationbar.tsx
+++ blockly-react/src/components/navbar/Navigationbar.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
 
 /**
  * A simple navigation bar that exists on every route of the site.
- * 
- * @returns a JSX containing a Navbar bootstrap component and its children
+ *
+ * @return {JSX.Element} a JSX containing
+ * a Navbar bootstrap component and its children
  * such as Navbar.Link and Navbar.Brand.
  */
 export default function Navigationbar() {
-
   return (
     <>
       <Navbar bg="primary" variant="dark" expand="lg">
@@ -26,15 +26,21 @@ export default function Navigationbar() {
               </LinkContainer>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
-  )
+  );
 }
--- blockly-react/src/components/navbar/index.tsx
+++ blockly-react/src/components/navbar/index.tsx
@@ -1,1 +1,1 @@
-export { default } from './Navigationbar'
+export {default} from './Navigationbar';
