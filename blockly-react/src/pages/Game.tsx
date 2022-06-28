af44c894d10622b90c90b31cc8f4623bdacc3a19
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-06-28 09:46:02 +0100

    Updates on pages: Code quality improvements

--- blockly-react/src/pages/Game.tsx
+++ blockly-react/src/pages/Game.tsx
@@ -1,32 +1,33 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from '../components/redux/store';
-import { Container, Row, Col } from 'react-bootstrap';
+import {Provider} from 'react-redux';
+import {store} from '../components/redux/store';
+import {Container, Row, Col} from 'react-bootstrap';
 import BlocklyComponent from '../components/blockly/BlocklyComponent';
 import Canvas from '../components/canvas/Canvas';
 
 /**
  * This functional component consists of the elements that are special to
  * "/game" route.
- * 
- * @returns a JSX template containing BlocklyComponent workspace component
+ *
+ * @return {JSX.Element} a JSX template
+ * containing BlocklyComponent workspace component
  * and Canvas component wrapped inside a Redux Provider.
  */
-export default function Game() {
+export default function Game(): JSX.Element {
   return (
     <>
-        <Provider store={store}>
-            <Container fluid>
-              <Row>
-                <Col>
-                  <BlocklyComponent />
-                </Col>
-                <Col>
-                  <Canvas width="800" height="600"/>
-                </Col>
-              </Row>
-            </Container>
-          </Provider>
+      <Provider store={store}>
+        <Container fluid>
+          <Row>
+            <Col>
+              <BlocklyComponent />
+            </Col>
+            <Col>
+              <Canvas width="800" height="600"/>
+            </Col>
+          </Row>
+        </Container>
+      </Provider>
     </>
-  )
+  );
 }
