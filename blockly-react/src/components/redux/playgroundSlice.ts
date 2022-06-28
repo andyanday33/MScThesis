07f65bd7e49fee878ca44df62614991aa8d3ac6b
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-06-28 09:43:29 +0100

    Update playgroundSlice: Code quality improvements

--- blockly-react/src/components/redux/playgroundSlice.ts
+++ blockly-react/src/components/redux/playgroundSlice.ts
@@ -1,29 +1,29 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {createSlice} from '@reduxjs/toolkit';
 
-/*TODO: add goal points for the game and reset positions to initial
+/* TODO: add goal points for the game and reset positions to initial
 if goal is not met */
 interface PlaygroundState {
     actors: number[][]
 }
 
 const initialState: PlaygroundState = {
-    //Actors on the map with their x,y coordinates
-    actors : [[50, 50], [50, 150], [50, 250]],
-}
+  // Actors on the map with their x,y coordinates
+  actors: [[50, 50], [50, 150], [50, 250]],
+};
 
 export const playgroundSlice = createSlice({
-    name: "actors",
-    initialState,
-    reducers: {
-        move: (state) => {
-            state.actors = state.actors.map((actor) => {
-                actor[0] += 50;
-                return actor;
-            });
-        },
-    }
+  name: 'actors',
+  initialState,
+  reducers: {
+    move: (state) => {
+      state.actors = state.actors.map((actor) => {
+        actor[0] += 50;
+        return actor;
+      });
+    },
+  },
 });
 
-export const { move } = playgroundSlice.actions;
+export const {move} = playgroundSlice.actions;
 
 export default playgroundSlice.reducer;
