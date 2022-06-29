3301a28983c9d3e82afa3cb6cecbd014b8fd99a7
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-06-29 13:00:16 +0100

    Added a boilerplate get started functional component

---
+++ blockly-react/src/pages/getStarted/GetStarted.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/**
+ * The functional component corresponding to
+ * Get Started page.
+ *
+ * @return {JSX.Element}
+ */
+export default function GetStarted(): JSX.Element {
+  return (
+    <div>GetStarted</div>
+  );
+}
