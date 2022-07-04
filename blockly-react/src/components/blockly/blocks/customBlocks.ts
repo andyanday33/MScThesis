a2ffe74522d7283318ef940fe75ac465ef07b232
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-07-04 15:26:39 +0100

    Code readability updates

--- blockly-react/src/components/blockly/blocks/customBlocks.ts
+++ blockly-react/src/components/blockly/blocks/customBlocks.ts
@@ -1,19 +1,19 @@
-import Blockly from 'blockly'
+import Blockly from 'blockly';
 
 Blockly.Blocks['move_forward_block'] = {
-    init: function() {
-      this.appendDummyInput()
-          .appendField("Move Forward");
-      this.setPreviousStatement(true, null);
-      this.setNextStatement(true, null);
-      this.setColour(230);
-   this.setTooltip("Moves the elements forward by one square");
-   this.setHelpUrl("");
-    }
-  };
+  init: function() {
+    this.appendDummyInput()
+        .appendField('Move Forward');
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip('Moves the elements forward by one square');
+    this.setHelpUrl('');
+  },
+};
 
-  Blockly.JavaScript['move_forward_block'] = function(block: any) {
-    // TODO: Assemble JavaScript into code variable.
-    var code = 'moveForward();\n';
-    return code;
-  };
+Blockly.JavaScript['move_forward_block'] = function(block: any) {
+  // TODO: Assemble JavaScript into code variable.
+  const code = 'moveForward();\n';
+  return code;
+};
