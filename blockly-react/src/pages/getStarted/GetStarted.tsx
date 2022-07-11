168b1a9489b733b51ac30ba97e4e1e45635a870c
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-07-11 17:15:00 +0100

    Added an accordion to get started page

--- blockly-react/src/pages/getStarted/GetStarted.tsx
+++ blockly-react/src/pages/getStarted/GetStarted.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import AlertDismissible from '../../components/alerts/AlertDismissable';
+import {Accordion} from 'react-bootstrap';
 /**
  * The functional component corresponding to
  * Get Started page.
@@ -8,6 +9,25 @@ import React from 'react';
  */
 export default function GetStarted(): JSX.Element {
   return (
-    <div>GetStarted</div>
+    <>
+      <AlertDismissible>
+      Welcome to Puzzlaffic guides page! You could find any knowledge
+       you need to play the game in this guide.
+      </AlertDismissible>
+      <Accordion>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Generating Code</Accordion.Header>
+          <Accordion.Body>
+            Hello there!
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Moving Actors Forward</Accordion.Header>
+          <Accordion.Body>
+            General Kenobi!
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </>
   );
 }
