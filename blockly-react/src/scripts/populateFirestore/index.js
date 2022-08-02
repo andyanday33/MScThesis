27e74d783600bea65eb4cb3c78ec409731af8466
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-08-02 09:03:31 +0100

    Added 4 new levels inside firestore population script

--- blockly-react/src/scripts/populateFirestore/index.js
+++ blockly-react/src/scripts/populateFirestore/index.js
@@ -1,9 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {initializeApp} from 'firebase/app';
 import {getFirestore} from 'firebase/firestore';
 import {doc, setDoc} from 'firebase/firestore';
 import {readFile} from 'fs/promises';
 import dotenv from 'dotenv';
 dotenv.config();
+
+const DIRECTION_EAST = 0;
+const DIRECTION_SOUTH = 1;
+const DIRECTION_WEST = 2;
+const DIRECTION_NORTH = 3;
 
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
@@ -28,9 +34,168 @@ const json = await JSON.parse(
     ),
 );
 
+// Levels 1-4
 for (const level of json.levels) {
-  console.log(level.levelNo);
   await setDoc(doc(db, 'levelsGenerated', `${level.levelNo}`), level);
 }
+
+// Level 5
+let level = {
+  gridSize: 10,
+  levelNo: 5,
+  actors: [
+    {
+      coordinateX: 1,
+      coordinateY: 2,
+      direction: DIRECTION_EAST,
+    },
+  ],
+  goals: [
+    {
+      coordinateX: 10,
+      coordinateY: 2,
+    },
+  ],
+  walls: [],
+  tip: 'Introducing: loops!',
+};
+for (let i = 1; i <= level.gridSize; i++) {
+  if (i == 2) {
+    continue;
+  } else {
+    for (let j = 1; j <= level.gridSize; j++) {
+      level.walls.push({
+        coordinateX: j,
+        coordinateY: i,
+      });
+    }
+  }
+}
+
+await setDoc(doc(db, 'levelsGenerated', `${level.levelNo}`), level);
+
+// Level 6
+level = {
+  gridSize: 10,
+  levelNo: 6,
+  actors: [
+    {
+      coordinateX: 10,
+      coordinateY: 10,
+      direction: DIRECTION_SOUTH,
+    },
+  ],
+  goals: [
+    {
+      coordinateX: 1,
+      coordinateY: 1,
+    },
+  ],
+  walls: [],
+  tip: 'Now try mixing loops with turning around and movement!',
+};
+for (let i = 1; i <= level.gridSize; i++) {
+  for (let j = 1; j <= level.gridSize; j++) {
+    console.log(Math.abs(i - j));
+    if (!(Math.abs(i - j) <= 1)) {
+      level.walls.push({
+        coordinateX: j,
+        coordinateY: i,
+      });
+    }
+  }
+}
+
+await setDoc(doc(db, 'levelsGenerated', `${level.levelNo}`), level);
+
+// Level 7
+level = {
+  gridSize: 10,
+  levelNo: 0,
+  actors: [
+    {
+      coordinateX: 1,
+      coordinateY: 10,
+      direction: DIRECTION_SOUTH,
+    },
+  ],
+  goals: [
+    {
+      coordinateX: 1,
+      coordinateY: 1,
+    },
+  ],
+  walls: [],
+  tip: 'more zig-zags!',
+};
+for (let i = 1; i <= level.gridSize / 2; i++) {
+  for (let j = 1; j <= level.gridSize / 2; j++) {
+    console.log(Math.abs(i - j));
+    if (i - j < 0 || i - j > 1) {
+      level.walls.push({
+        coordinateX: j,
+        coordinateY: i,
+      });
+    }
+  }
+}
+for (let i = level.gridSize; i > level.gridSize / 2; i--) {
+  for (let j = 1; j <= level.gridSize / 2; j++) {
+    if (i + j - level.gridSize < 0 || i + j - level.gridSize > 1) {
+      level.walls.push({
+        coordinateX: j,
+        coordinateY: i,
+      });
+    }
+  }
+}
+for (let i = 1; i <= level.gridSize; i++) {
+  for (let j = 6; j <= level.gridSize; j++) {
+    level.walls.push({
+      coordinateX: j,
+      coordinateY: i,
+    });
+  }
+}
+
+await setDoc(doc(db, 'levelsGenerated', `${level.levelNo}`), level);
+
+// Level 8
+level = {
+  gridSize: 10,
+  levelNo: 8,
+  actors: [
+    {
+      coordinateX: 1,
+      coordinateY: 10,
+      direction: DIRECTION_SOUTH,
+    },
+  ],
+  goals: [
+    {
+      coordinateX: 10,
+      coordinateY: 5,
+    },
+  ],
+  walls: [],
+  tip: 'This is where the fun begins.',
+};
+for (let i = 1; i <= level.gridSize / 2; i++) {
+  for (let j = 1; j <= level.gridSize / 2; j++) {
+    console.log(Math.abs(i - j));
+    if (!(Math.abs(i - j) <= 1)) {
+      level.walls.push({
+        coordinateX: j,
+        coordinateY: i,
+      });
+      level.walls.push({
+        coordinateX: level.gridSize - j,
+        coordinateY: level.gridSize - i,
+      });
+    }
+  }
+}
+
+await setDoc(doc(db, 'levelsGenerated', `${level.levelNo}`), level);
 
 process.abort();
